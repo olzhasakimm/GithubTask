a7bcf992c9fd1ed799a0027d5c5190b177384136
Olzhas-Akim <220107039.@stu.sdu.edu.kz>
2022-12-03 13:00:36 +0600

    Solution

---
+++ Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public static void main(String [] args){
+        System.out.println("Name: Olzhas");
+        System.out.println("Surname: Akimbay");
+        System.out.println("Id:220107039");
+        System.out.println("Hobby: playing, sleeping, reading");
+        System.out.println("How much do you like ICT(from 1-10): 9");
+        System.out.println("Rate this task from 1 to 10 : 7");
+        System.out.println("Am I the best the teacher?: Normal");    
+    }
+}
